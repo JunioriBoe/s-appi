3125d1ef1600e221a03fce4af0734f99c4bf3605
JunioriBoe <kodarit2020@gmail.com>
2020-10-20 18:15:03 +0300

    emt mitä tehtiin

--- saa-appi/src/Weather.js
+++ saa-appi/src/Weather.js
@@ -5,11 +5,35 @@ class Weather extends Component {
       return(
         <div className="Weather">
           <div className="Weather_focused">
-            <div className="Weather_day">ma</div>
+            <div className="Weather_day">pe</div>
             <div className="Weather_time">12:00</div>
             <div className="Weather_day">14°</div>
              <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
           </div>
+
+          <div className="Weather_forecast">
+
+            <div className="Weather_box">
+              <div className="Weather_day">ma</div>
+              <div className="Weather_time">10:00</div>
+              <div className="Weather_day">16°</div>
+              <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+            </div>
+             <div className="Weather_box">
+               <div className="Weather_day">ma</div>
+               <div className="Weather_time">11:00</div>
+               <div className="Weather_day">15°</div>
+               <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+             </div>
+              <div className="Weather_box">
+                <div className="Weather_day">ma</div>
+                <div className="Weather_time">12:00</div>
+                <div className="Weather_day">14°</div>
+                <img src='http://openweathermap.org/img/wn/09d.png' alt="logo" />
+              </div>
+
+          </div>
+
         </div>
       )
   }

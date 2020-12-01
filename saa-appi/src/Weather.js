5087ab433214c46c49395caf46e81c6251d76286
simeon <simeon>
2020-12-01 18:17:01 +0200

    tehtiin nappulajuttui

--- saa-appi/src/Weather.js
+++ saa-appi/src/Weather.js
@@ -6,12 +6,13 @@ class Weather extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      forecasts: null
+      forecasts: null,
+      focusedForecastDt: null
     }
   }
   componentDidMount() {
      getForecast().then(forecasts => {
-       this.setState({ forecasts: forecasts })
+       this.setState({ forecasts: forecasts, focusedForecastDt: forecasts.list[0].dt })
      })
    }
 
@@ -34,7 +35,7 @@ class Weather extends Component {
 
    renderWeatherItem(forecast) {
      return (
-       <div className="Weather_box">
+       <div className="Weather_box" key={forecast.Dt} onClick={() => this.changeFocusedForecast(forecast)}>
          <div className="Weather_day">{this.formatDay(forecast.dt)}</div>
          <div className="Weather_time">{this.formatTime(forecast.dt)}</div>
          <div className="Weather_temp">{Math.round(forecast.main.temp)}°</div>
@@ -43,12 +44,17 @@ class Weather extends Component {
      )
    }
 
+   changeFocusedForecast = forecast => {
+     this.setState({focusedForecastDt: forecast.dt})
+   }
+
    render() {
      const forecasts = this.state.forecasts
+     const focusedForecastDt = this.state.focusedForecastDt
      if (!forecasts) return null
      console.log(forecasts)
 
-     const focusedWeather = forecasts.list[0]
+     const focusedWeather = forecasts.list.find(f => f.dt === focusedForecastDt)
 
      return (
        <div className="Weather">
@@ -56,6 +62,7 @@ class Weather extends Component {
           <div className="Weather_day">{this.formatDay(focusedWeather.dt)}</div>
           <div className="Weather_time">{this.formatTime(focusedWeather.dt)}</div>
           <div className="Weather_temp">{Math.round(focusedWeather.main.temp)}°</div>
+          <div className="Weather_desc">{focusedWeather.weather[0].description}</div>
           <img src={'http://openweathermap.org/img/wn/'+focusedWeather.weather[0].icon+'.png'} alt="Logo" />
       </div>
 
